805b2593827914f7f90fdf29255ef9326b5f08e3
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-09-05 16:01:19 -0300

    Add files via upload

---
+++ UniExpo/javascript.js
@@ -0,0 +1,19 @@
+function abrirMenu() {
+    document.getElementById("navbar").style.width = "250px";
+}
+
+function fecharMenu() {
+    document.getElementById("navbar").style.width = "0";
+}
+
+function redirecionarLogin(){
+    location.href="login.html";
+}
+
+function redirecionarCadastro(){
+    location.href="cadastro.html";
+}
+
+function redirecionarCursos(){
+    location.href="cursos.html";
+}
