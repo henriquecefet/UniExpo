1e210ec9bc5a873a9b59fbdbdcbd378ac2e9da2d
HenriqueSoaresRodrigues <rodriguesoareshenrique@gmail.com>
2022-09-05 19:24:50 -0300

    Testar db

--- cadastrarperfil.php
+++ cadastrarperfil.php
@@ -9,7 +9,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
      INSERT INTO public.curso (nomme, email ,curso ,hashsenha)
      VALUES ('$nome', '$email', '$curso',  '$hashsenha' );
 EOF;
-  $ret = pg_query($_SESSION['db'], $sql);
+echo isset($db);
+  $ret = pg_query($db, $sql);
   if(!$ret) {
      http_response_code(501);
   } else {
--- conexao.php
+++ conexao.php
@@ -3,9 +3,8 @@ $host        = "host = ec2-34-233-115-14.compute-1.amazonaws.com";
 $port        = "port = 5432";
 $dbname      = "dbname = dbausclhsrca45";
 $credentials = "user = xmgknnbnrrfsip password=8d4babda99903940e95e1df59f23f9adead22c1ec99585f19a771e85711a5f68";
-session_start();
-   $_SESSION['db'] = pg_connect( "$host $port $dbname $credentials"  );;
-	if(!$_SESSION['db']) {
+$db = pg_connect( "$host $port $dbname $credentials"  );
+	if(!$db) {
 	      echo "Error : Unable to open database\n";
 	 } else {
 	     echo "Opened database successfully\n";
