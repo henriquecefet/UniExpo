b4a241b826cfd0b3597954bc3c11a7dcaaa9abfb
HenriqueSoaresRodrigues <rodriguesoareshenrique@gmail.com>
2022-09-06 17:53:17 -0300

    Atualizar credenciais

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host        = "host = ec2-34-233-115-14.compute-1.amazonaws.com";
+$host        = "host = ec2-54-152-28-9.compute-1.amazonaws.com";
 $port        = "port = 5432";
-$dbname      = "dbname = dbausclhsrca45";
-$credentials = "user = xmgknnbnrrfsip password=8d4babda99903940e95e1df59f23f9adead22c1ec99585f19a771e85711a5f68";
+$dbname      = "dbname = d4a98bg3enk2n2";
+$credentials = "user = ekpyivbnwyisml password=52b262b77718ccdef3b6a0b90181bd5669a8c9f03b01fc03a720e25e1ab42b53";
 $db = pg_connect( "$host $port $dbname $credentials"  );
 	if(!$db) {
 	      echo "Error : Unable to open database\n";
