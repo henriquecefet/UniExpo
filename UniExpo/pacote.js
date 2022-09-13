2a4ecb26bba75408293f4aeeedeb4041716696aa
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-09-12 22:20:42 -0300

    Update pacote.js

--- UniExpo/pacote.js
+++ UniExpo/pacote.js
@@ -1,31 +1,3 @@
-var setarInfo = function(){
-    console.log("entrei no setarInfo");
-    localStorage.setItem("email",document.getElementById("email").value);
-    console.log(document.getElementById("email").value);
-}
-var pegarInfo =  function(){
-    localStorage.getItem("email");
-    let email = localStorage.getItem("email");
-    var settings = {
-        "url": "https://projetouniexpo.herokuapp.com/recuperaPerfil.php?email=" + email,
-        "method": "GET",
-        "timeout": 0,
-        "processData": false,
-        "mimeType": "multipart/form-data",
-        "contentType": false,
-    };
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-        var jx = JSON.parse(response);
-        console.log(jx.perfis[0].nome);
-        document.getElementById("nome").innerHTML = jx.perfis[0].nome;
-
-
-    });
-
-}
-
 function abrirMenu() {
     document.getElementById("navbar").style.width = "250px";
 }
@@ -44,4 +16,32 @@ function redirecionarCadastro(){
 
 function redirecionarCursos(){
     location.href="cursos.html";
+}
+
+function pegarInfo(){
+    let email = localStorage.getItem("email");
+    var settings = {
+        "url": "https://projetouniexpo.herokuapp.com/recuperaPerfil.php?email=" + email,
+        "method": "GET",
+        "timeout": 0,
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var jx = JSON.parse(response);
+        console.log(jx.perfis[0].nome);
+        document.getElementById("nome").innerHTML = jx.perfis[0].nome;
+        document.getElementById("curso").innerHTML = jx.perfis[0].curso;
+
+    })
+
+}
+
+function setarInfo(){
+    console.log("entrei no setarInfo");
+    localStorage.setItem("email",document.getElementById("email").value);
+    console.log(document.getElementById("email").value);
 }
