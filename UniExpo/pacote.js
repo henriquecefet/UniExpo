9f4a6bfb99b24094218ff03e5ed7d440b6807c5b
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-09-12 22:49:08 -0300

    Update pacote.js

--- UniExpo/pacote.js
+++ UniExpo/pacote.js
@@ -7,7 +7,7 @@ var pegarInfo =  function(){
     localStorage.getItem("email");
     let email = localStorage.getItem("email");
     var settings = {
-        "url": "https://projetouniexpo.herokuapp.com/recuperaPerfil.php?email=baby@gmail.com"
+        "url": "https://projetouniexpo.herokuapp.com/recuperaPerfil.php?email=baby@gmail.com",
         "method": "GET",
         "timeout": 0,
         "processData": false,
