d93289ff5a5a20b47b49b29e0e6739bd1a192e62
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2022-09-12 21:29:43 -0300

    Update javascript.js

--- UniExpo/javascript.js
+++ UniExpo/javascript.js
@@ -1,22 +1,3 @@
-function abrirMenu() {
-    document.getElementById("navbar").style.width = "250px";
-}
-
-function fecharMenu() {
-    document.getElementById("navbar").style.width = "0";
-}
-
-function redirecionarLogin(){
-    location.href="login.html";
-}
-
-function redirecionarCadastro(){
-    location.href="cadastro.html";
-}
-
-function redirecionarCursos(){
-    location.href="cursos.html";
-}
 
 function PegarInfo() {
 
@@ -39,6 +20,26 @@ function PegarInfo() {
 
     })
 
+}
+
+function abrirMenu() {
+    document.getElementById("navbar").style.width = "250px";
+}
+
+function fecharMenu() {
+    document.getElementById("navbar").style.width = "0";
+}
+
+function redirecionarLogin(){
+    location.href="login.html";
+}
+
+function redirecionarCadastro(){
+    location.href="cadastro.html";
+}
+
+function redirecionarCursos(){
+    location.href="cursos.html";
 }
 
 function setarInfo(){
