9d95ce4bd40d25aa6d077284ea789bedcf8fbb59
HenriqueSoaresRodrigues <rodriguesoareshenrique@gmail.com>
2022-09-23 17:50:24 -0300

    CadastrarProjeto

--- cadastrarprojeto.php
+++ cadastrarprojeto.php
@@ -3,12 +3,28 @@ include("conexao.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
   $nome= $_REQUEST["nome"];
   $descricaoProjeto = $_REQUEST["descricaoProjeto"];
-  $nomeOrientador = md5($_REQUEST["nomeOrientador"]);
+  $nomeOrientador = $_REQUEST["nomeOrientador"];
+  $idPerfil = $_REQUEST["idperfil"];
   $sql =<<<EOF
      INSERT INTO public.projeto (nome, descricao ,orientador)
      VALUES ('$nome', '$descricaoProjeto', '$nomeOrientador' );
 EOF;
   $ret = pg_query($db, $sql);
+
+$sql2 =<<<EOF
+     SELECT idprojeto FROM public.projeto where nome = $nome;
+EOF;
+  $ret2 = pg_query($db, $sql2);
+  while($row = pg_fetch_row($ret2)) {
+    $idprojeto = $row[0];
+  }
+
+  $sq3 =<<<EOF
+     INSERT INTO public.perfil_projeto (perfil_idperfil,projeto_idprojeto)
+     VALUES ($idPerfil, $idprojeto, '$nomeOrientador' );
+EOF;
+  $ret3 = pg_query($db, $sql3);
+
   if(!$ret) {
      http_response_code(501);
   } else {
