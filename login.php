c458fbc6e7c6efd6002181c51d5771418dbc2ac4
Henrique Soares Rodrigues <rodriguesoareshenrique@gmail.com>
2022-09-23 14:35:16 -0300

    Update login.php

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ EOF;
     }
     if($podeir){
       echo json_encode($response);
-      echo '<script src="pacote.js"> setarInfo() </script>'
+      echo '<script src="pacote.js"> setarInfo() </script>';
       header('Location: perfil.html');
     }
     else{
