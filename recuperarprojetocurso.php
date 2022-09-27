216216a68a4564c8bd61081eeb3aab75882a79bb
HenriqueSoaresRodrigues <rodriguesoareshenrique@gmail.com>
2022-09-27 16:44:46 -0300

    Link curso

--- recuperarprojetocurso.php
+++ recuperarprojetocurso.php
@@ -3,7 +3,11 @@ include("conexao.php");
 if ($_SERVER["REQUEST_METHOD"] == "GET"){
   $idcurso = $_GET["curso"];
   $sql =<<<EOF
-    select projeto.*, curso.nome from public.projeto join perfil_projeto on (projeto_idprojeto = idprojeto) join perfil on (perfil_idperfil = idperfil) join curso on (curso_idcurso = idcurso and idcurso =   $idcurso);
+  select projeto.*, curso.nome, endereco from link
+  join public.projeto on (projeto.idprojeto = link.projeto_idprojeto)
+  join perfil_projeto on (perfil_projeto.projeto_idprojeto = projeto.idprojeto)
+  join perfil on (perfil_projeto.perfil_idperfil =  perfil.idperfil)
+  join curso on (perfil.curso_idcurso = curso.idcurso and curso.idcurso = idcurso);
 EOF;
   $ret = pg_query($db, $sql);
   if(!$ret) {
@@ -18,6 +22,7 @@ EOF;
       $projeto["orientador"] = $row[2];
       $projeto["idprojeto"] = $row[3];
       $projeto["curso"] = $row[4];
+      $projeto["link"] = $row[5];
       array_push($response["projetos"], $projeto);
     }
     echo json_encode($response);
